993741cb761d687a134e38d13172ca0ad866b210
AwaisDotGeek <awais.z.love348@gmail.com>
2023-09-17 20:45:39 +0500

    Welcome to laravel - Inertia

---
+++ resources/js/pages/basic/Welcome.vue
@@ -0,0 +1,74 @@
+<template>
+    <div class="home-container">
+        <div class="w-screen h-screen flex justify-center items-center">
+            <img src="../../assets/bg.png" alt="" class="border-0">
+        </div>
+        <div class="bg-img-shadow"></div>
+
+        <div class="absolute top-0 left-0 w-full h-full flex justify-center items-center">
+            <div class="bg-404a4c text-white p-8 rounded-lg opacity-95 grid place-items-center">
+                <div class="w-20 h-20 mb-4 rounded-full overflow-hidden">
+                    <img src="../../assets/logo.png" alt="">
+                </div>
+                <h3>With us, YOU play games!</h3>
+                <div class="flex justify-center gap-2.5 mt-3">
+                    <Link href="/signup">Signup</Link>
+                    <Link href="/login">Login</Link>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup> 
+    import { Link } from '@inertiajs/vue3';
+</script>
+
+<style scoped>
+
+.bg-404a4c {
+    background: #404a4c;
+}
+
+h3 {
+    letter-spacing: 0.5px;
+}
+
+a {
+    text-decoration: none;
+    color: #fff;
+    letter-spacing: 0.7px;
+    background: #0e0f11;
+    padding: 8px 10px;
+    border-radius: 5px;
+
+    width: 100px;
+    text-align: center;
+    transition: all 0.25s ease;
+}
+
+a:hover {
+    transform: translateY(-2px);
+    box-shadow: 0 0 10px 2px #333738;
+}
+
+a:active {
+    transform: translateY(2px);
+}
+
+
+/* Shadow over bg image */
+.bg-img-shadow {
+    position: absolute;
+    top: 0;
+    left: 0;
+    background: #000;
+    width: 100%;
+    height: 100%;
+    opacity: 0.3;
+}
+</style>
+
+
+
+
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('basic/Welcome');
 });
